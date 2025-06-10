68c930f93af3c6c30e18a244f3dc116255fe9723
ayush <ayush.anneya@cloudnexus.in>
2025-06-10 16:51:27 +0530

    check

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello from Docker container!\n');
 }).listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`hello Server running at http://localhost:${PORT}`);
 });
